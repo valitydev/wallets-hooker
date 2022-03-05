8cd4649a8f4bfd454f9cb2d81247da0eb18857a4
dmitriibaikov <bdv@empayre.com>
2022-03-05 15:47:10 +0300

    TD-233: Fix ObjectMapper deserialization

--- src/main/java/dev/vality/wallets/hooker/config/ObjectMapperConfig.java
+++ src/main/java/dev/vality/wallets/hooker/config/ObjectMapperConfig.java
@@ -1,6 +1,7 @@
 package dev.vality.wallets.hooker.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
@@ -18,6 +19,7 @@ public class ObjectMapperConfig {
                 .registerModule(new ParameterNamesModule())
                 .registerModule(new Jdk8Module())
                 .registerModule(new JavaTimeModule())
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                 .setSerializationInclusion(JsonInclude.Include.NON_EMPTY)
                 .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
