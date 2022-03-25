76ae33f04dcf55c0bf20d65932453420240c9828
Kostya Struga <kennny007@yandex.ru>
2022-03-25 15:43:15 +0300

    Fix tests

--- src/test/java/dev/vality/wallets/hooker/converter/DestinationToDestinationMessageConverterTest.java
+++ src/test/java/dev/vality/wallets/hooker/converter/DestinationToDestinationMessageConverterTest.java
@@ -5,92 +5,105 @@ import dev.vality.fistful.destination.Destination;
 import dev.vality.fistful.msgpack.Value;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DestinationToDestinationMessageConverterTest {
 
     private DestinationToDestinationMessageConverter converter = new DestinationToDestinationMessageConverter();
+    private ResourceToJsonStringDestinationConverter resourceToJsonStringDestinationConverter
+            = new ResourceToJsonStringDestinationConverter();
 
     @Test
     public void testConvertFromEventWithBankCardResource() {
+        Resource resource = Resource.bank_card(
+                new ResourceBankCard(
+                        new BankCard("token")
+                                .setBin("bin")
+                                .setMaskedPan("masked_pan")
+                                .setCardType(CardType.charge_card)
+                                .setBinDataId(Value.i(1))
+                                .setPaymentSystem(new PaymentSystemRef(
+                                        LegacyBankCardPaymentSystem.mastercard.name()))
+                )
+        );
         Destination destination = new Destination()
                 .setName("name")
                 .setExternalId("external_id")
-                .setResource(Resource.bank_card(
-                        new ResourceBankCard(
-                                new BankCard("token")
-                                        .setBin("bin")
-                                        .setMaskedPan("masked_pan")
-                                        .setCardType(CardType.charge_card)
-                                        .setBinDataId(Value.i(1))
-                                        .setPaymentSystem(new PaymentSystemRef(
-                                                LegacyBankCardPaymentSystem.mastercard.name()))
-                        )
-                ));
+                .setResource(resource);
         var swagDestination = converter.convert(destination);
         assertEquals(destination.getExternalId(), swagDestination.getExternalID());
         assertEquals(destination.getName(), swagDestination.getName());
-        var bankCard = (dev.vality.swag.wallets.webhook.events.model.BankCard) swagDestination.getResource();
-        assertNotNull(bankCard.getPaymentSystem());
+
+        String resultString = resourceToJsonStringDestinationConverter.convert(resource);
+        assertEquals("{\"type\":\"BankCard\",\"cardNumberMask\":\"masked_pan\",\"bin\":\"bin\"," +
+                "\"lastDigits\":null,\"paymentSystem\":\"mastercard\",\"tokenProvider\":null}", resultString);
     }
 
     @Test
     public void testConvertFromEventWithCryptoWalletResource() {
+        Resource resource = Resource.crypto_wallet(
+                new ResourceCryptoWallet(
+                        new CryptoWallet("crypto_wallet_id", CryptoCurrency.bitcoin_cash)
+                                .setData(CryptoData.bitcoin_cash(new CryptoDataBitcoinCash()))
+                )
+        );
         Destination destination = new Destination()
                 .setName("name")
                 .setExternalId("external_id")
-                .setResource(Resource.crypto_wallet(
-                        new ResourceCryptoWallet(
-                                new CryptoWallet("crypto_wallet_id", CryptoCurrency.bitcoin_cash)
-                                        .setData(CryptoData.bitcoin_cash(new CryptoDataBitcoinCash()))
-                        )
-                ));
+                .setResource(resource);
         var swagDestination = converter.convert(destination);
         assertEquals(destination.getExternalId(), swagDestination.getExternalID());
         assertEquals(destination.getName(), swagDestination.getName());
-        var cryptoWallet = (dev.vality.swag.wallets.webhook.events.model.CryptoWallet) swagDestination.getResource();
-        assertNotNull(cryptoWallet.getCurrency());
+
+        String resultString = resourceToJsonStringDestinationConverter.convert(resource);
+        assertEquals("{\"type\":\"CryptoWallet\",\"cryptoWalletId\":\"crypto_wallet_id\"," +
+                "\"currency\":\"BitcoinCash\"}", resultString);
     }
 
     @Test
     public void testConvertFromEventWithDigitalWalletResource() {
+        Resource digitalWalletId = Resource.digital_wallet(
+                new ResourceDigitalWallet((
+                        new DigitalWallet("digital_wallet_id", new PaymentServiceRef().setId("123"))
+                ))
+        );
         Destination destination = new Destination()
                 .setName("name")
                 .setExternalId("external_id")
-                .setResource(Resource.digital_wallet(
-                        new ResourceDigitalWallet((
-                                new DigitalWallet("digital_wallet_id", new PaymentServiceRef().setId("123"))
-                        ))
-                ));
+                .setResource(digitalWalletId);
         var swagDestination = converter.convert(destination);
         assertEquals(destination.getExternalId(), swagDestination.getExternalID());
         assertEquals(destination.getName(), swagDestination.getName());
-        var digitalWallet = (dev.vality.swag.wallets.webhook.events.model.DigitalWallet) swagDestination
-                .getResource();
-        assertNotNull(digitalWallet.getDigitalWalletId());
-        assertNotNull(digitalWallet.getDigitalWalletProvider());
+
+        String digitalWalletString = resourceToJsonStringDestinationConverter.convert(digitalWalletId);
+        assertEquals(
+                "{\"type\":\"DigitalWallet\",\"digitalWalletId\":\"digital_wallet_id\",\"digitalWalletProvider\":\"123\"}",
+                digitalWalletString);
     }
 
     @Test
     public void testConvertFromEventWithBankCardResourceAndPaymentSystemIsNull() {
+        Resource resource = Resource.bank_card(
+                new ResourceBankCard(
+                        new BankCard("token")
+                                .setBin("bin")
+                                .setMaskedPan("masked_pan")
+                                .setCardType(CardType.charge_card)
+                                .setBinDataId(Value.i(1))
+                                .setPaymentSystem(null)
+                )
+        );
         Destination destination = new Destination()
                 .setName("name")
                 .setExternalId("external_id")
-                .setResource(Resource.bank_card(
-                        new ResourceBankCard(
-                                new BankCard("token")
-                                        .setBin("bin")
-                                        .setMaskedPan("masked_pan")
-                                        .setCardType(CardType.charge_card)
-                                        .setBinDataId(Value.i(1))
-                                        .setPaymentSystem(null)
-                        )
-                ));
+                .setResource(resource);
         var swagDestination = converter.convert(destination);
         assertEquals(destination.getExternalId(), swagDestination.getExternalID());
         assertEquals(destination.getName(), swagDestination.getName());
-        var bankCard = (dev.vality.swag.wallets.webhook.events.model.BankCard) swagDestination.getResource();
-        assertNull(bankCard.getPaymentSystem());
+
+        String resultString = resourceToJsonStringDestinationConverter.convert(resource);
+        assertEquals("{\"type\":\"BankCard\",\"cardNumberMask\":\"masked_pan\",\"bin\":\"bin\"," +
+                "\"lastDigits\":null,\"paymentSystem\":null,\"tokenProvider\":null}", resultString);
     }
 
 }
